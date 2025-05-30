ce4ad7ea5882525ceee9ab1c63601440b1f01f64
Mohd Saif <mohdsaifsiddique.feaindia@gmail.com>
2025-05-30 12:46:50 +0530

    May 30
    jQuery syntax and events

---
+++ JavaScript/jQuery/may-30/index.js
@@ -0,0 +1,30 @@
+// $(document).ready(function () {
+//   $("p").click(function () {
+//     $("h1").toggle();
+//   });
+// });
+
+/* 
+When we work with functions we should know the following terms
+Function name
+function declaration
+function definition
+function parameter
+function argument
+callback function
+function return value
+*/
+
+/* 
+function fName (param1, parm2, ...) {
+  // Codes to execute in a function
+  return fReturnValue;
+} 
+*/
+
+function sum(a, b) {    // Function declaration
+  let c = a + b;        // Function definition
+  return c;
+}
+
+let result = sum(5, 2); // Function call
